b6f2b0a016f120c24f219b07ef384317b641e9f5
aalu1418 <aalu1418@gmail.com>
2020-02-01 13:33:49 -0500

    finished assignment

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,7 +8,6 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 const web3 = new Web3("http://localhost:8545");
-let accounts;
 const stockQuote = new web3.eth.Contract(
   STOCK_ORACLE_ABI,
   STOCK_ORACLE_ADDRESS
@@ -18,19 +17,16 @@ function App() {
   const [stock, setStock] = React.useState("");
   const [data, setData] = React.useState({});
   const [accounts, setAccounts] = React.useState([]);
+  const [price, setPrice] = React.useState(null);
+  const [volume, setVolume] = React.useState(null);
 
   React.useEffect(() => {
     const getAccounts = async () => {
       const accountsTemp = await web3.eth.getAccounts();
-      setAccounts(accountsTemp)
-      // console.log("Account 0 = ", accounts[0]);
-      // var retval = await stockQuote.methods
-      //   .getStockPrice(web3.utils.fromAscii("AAAA"))
-      //   .call();
-      // console.log(retval);
+      setAccounts(accountsTemp);
     };
     getAccounts();
-  }, [])
+  }, []);
 
   React.useEffect(() => {
     const writeToContract = async () => {
@@ -39,38 +35,60 @@ function App() {
         Number(data["05. price"]) * 100,
         Number(data["06. volume"])
       );
-      const output = await stockQuote.methods
+      await stockQuote.methods
         .setStock(
           web3.utils.fromAscii(stock),
-          Number(data["05. price"]) * 100,
+          Math.round(Number(data["05. price"]) * 100),
           Number(data["06. volume"])
         )
-        .send({from: accounts[0]});
-      setStock("");
+        .send({ from: accounts[0] });
       setData({});
     };
-    // testFunc();
+
+    const readFromContract = async () => {
+      var retPrice = await stockQuote.methods
+        .getStockPrice(web3.utils.fromAscii(stock))
+        .call();
+      var retVolume = await stockQuote.methods
+        .getStockVolume(web3.utils.fromAscii(stock))
+        .call();
+
+      setVolume(Number(retVolume));
+      setPrice(retPrice/100);
+      // console.log(retPrice, retVolume);
+    };
 
     if (Object.keys(data).length !== 0 && stock.length !== 0) {
       writeToContract();
+      setTimeout(readFromContract, 500); //let ganache commit transaction
     }
-  }, [data, stock]);
+  }, [data, stock, accounts]);
 
   const getAPIdata = async () => {
     if (stock.length !== 0) {
       // console.log(stock);
       // console.log(process.env.REACT_APP_STOCK_API_KEY);
-      const res = await fetch(
+      await fetch(
         `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${process.env.REACT_APP_STOCK_API_KEY}`
       )
         .then(res => res.json())
         .then(data => {
-          setData(data["Global Quote"]);
-          // console.log(data["Global Quote"]);
+          if (data["Global Quote"]) {
+            setData(data["Global Quote"]);
+          }
         })
         .catch(console.log);
     }
   };
+
+  const printData = (
+    <div className="output-data">
+      <p>
+        {`Price: ${price}`}
+      </p>
+      <p> {`Volume: ${volume}`}</p>
+    </div>
+  )
 
   return (
     <div className="App">
@@ -91,6 +109,7 @@ function App() {
         </Form.Group>
       </Form>
       <Button onClick={getAPIdata}>Submit</Button>
+      {price !== null && volume !== null && printData}
     </div>
   );
 }
